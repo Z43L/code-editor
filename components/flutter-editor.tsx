413463e87e2a45fa4fbbe1531c9543f5059df6c6
david moreno <davidmoreno@macbook-air-de-david.home>
2025-09-29 11:32:27 +0200

    update

--- components/flutter-editor.tsx
+++ components/flutter-editor.tsx
@@ -76,7 +76,7 @@ codigo pero por muy simple que parezca ahi es donde
 reside su fuerza
 
 que la fuerza te acompañe
-          **Anaquin Skywaler**.
+          **Anakin Skywalker**.
 `
 
 export function FlutterEditor() {
